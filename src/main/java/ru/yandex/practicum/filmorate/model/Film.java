a14d3d72f9a74ae0b9885f5252af583b93a7f7a7
n20va <99407852+n20va@users.noreply.github.com>
2025-09-01 10:34:52 +0300

    Update Film.java

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,9 +3,9 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Builder;
 import lombok.Data;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-import jakarta.validation.constraints.Positive;
+import javac.validation.constraints.NotBlank;
+import javac.validation.constraints.Size;
+import javac.validation.constraints.Positive;
 import java.time.LocalDate;
 
 @Data
